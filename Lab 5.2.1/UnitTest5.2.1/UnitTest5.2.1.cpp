35f89bfc7ba6edad3a0b2d19f019677fc8cfc9a5
Night0infinity <l.kurylo2005@gmail.com>
2022-11-04 01:12:28 +0200

    first

---
+++ Lab 5.2.1/UnitTest5.2.1/UnitTest5.2.1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab 5.2.1/Source.cpp"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest521
+{
+	TEST_CLASS(UnitTest521)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double m = A(1, 0, -1);
+			Assert::AreEqual(m, -1.);
+		}
+	};
+}
---
+++ Lab 5.2/UnitTest 5.2/UnitTest 5.2.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab 5.2/Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest52
+{
+	TEST_CLASS(UnitTest52)
+	{
+	public:
+		
+		TEST_METHOD(TestMethodA)
+		{
+			double a = -1.;
+			A(1, 0, a);
+			Assert::AreEqual(a, -1.);
+		}
+	};
+}
